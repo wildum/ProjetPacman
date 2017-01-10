ad3e72e1c14811b8d4be337b74bb72c9e7b17af8
Eliott <Eliott@Lenovo-PC-Eliott>
2017-01-10 23:25:22 +0100

    Run-17
    Modification du menu principal et création du menu contrôle (classe
    GestionDuJeu) :
    - menu principal : changement des interactions avec la souris (les mots
    s'agrandissent et changent de couleur)
    - menu contrôle : ajout de la liste des commandes du jeu

--- src/GestionDuJeu.java
+++ src/GestionDuJeu.java
@@ -1,6 +1,11 @@
+import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 
@@ -8,6 +13,7 @@ public class GestionDuJeu {
 	
 	// Chronomètre du jeu :
 	static Chrono chrono = new Chrono();
+	static Font petite = new Font("SHOWCARD GOTHIC", Font.BOLD, 18), normale = new Font("SHOWCARD GOTHIC", Font.BOLD, 20), selection = new Font("SHOWCARD GOTHIC", Font.BOLD, 22), grande = new Font("SHOWCARD GOTHIC", Font.BOLD, 40);
 	
 	public static Chrono getChrono() {
 		return chrono;
@@ -70,9 +76,8 @@ public class GestionDuJeu {
 		int choixDuJoueur = 0;
 		StdDraw.setCanvasSize(500,760); 
 		StdDraw.setXscale(-2,110);
-		StdDraw.setYscale(-27,149); 
-		Font font = new Font("SHOWCARD GOTHIC", Font.BOLD, 20);
-		StdDraw.setFont(font);
+		StdDraw.setYscale(-27,149);
+		StdDraw.setFont(normale);
 		StdDraw.clear(StdDraw.BLACK);
 		StdDraw.picture(54,114,"Images/TitreMenu.png",80,30);
 		
@@ -80,109 +85,130 @@ public class GestionDuJeu {
 		{
 		while(StdDraw.mousePressed() == false)
 		{
-			cliqueCase = true;
+			cliqueCase = true;			
 			
 			// ONE PLAYER :
-			if(StdDraw.mouseX() > 24.5 && StdDraw.mouseX() < 84.5 && StdDraw.mouseY() > 76 && StdDraw.mouseY() < 86) // choix du mode 1 joueur
+			if(StdDraw.mouseX() > 39.5 && StdDraw.mouseX() < 69 && StdDraw.mouseY() > 36 && StdDraw.mouseY() < 40.5) // choix du mode 1 joueur
 			{
+				StdDraw.pause(50);
 				choixDuJoueur = 1;
 				cliqueCase = false;
-				StdDraw.setPenColor(StdDraw.YELLOW);
-				StdDraw.filledRectangle(54.5, 76, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 34, 30, 5);
+				StdDraw.setFont(selection);
+				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.text(54, 31.8, "One Player");
+				
 			}
 			else
 			{
-				StdDraw.setPenColor(StdDraw.BLUE);
-				StdDraw.filledRectangle(54.5, 76, 30, 5);
+				StdDraw.pause(50);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 34, 30, 3.5);
+				StdDraw.setFont(normale);
+				StdDraw.setPenColor(200,200,200);
+				StdDraw.text(54, 32, "One Player");
 			}
 			
 			// TWO PLAYER :
-			if(StdDraw.mouseX() > 24.5 && StdDraw.mouseX() < 84.5 && StdDraw.mouseY() > 56 && StdDraw.mouseY() < 66) // choix du mode 2 joueurs
+			if(StdDraw.mouseX() > 37.2 && StdDraw.mouseX() < 71.1 && StdDraw.mouseY() > 26.3 && StdDraw.mouseY() < 30) // choix du mode 2 joueurs
 			{
 				choixDuJoueur = 2;
 				cliqueCase = false;
-				StdDraw.setPenColor(StdDraw.YELLOW);
-				StdDraw.filledRectangle(54.5, 56, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 25, 30, 3.5);
+				StdDraw.setFont(selection);
+				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.text(54, 21.77, "Two Players");
 			}
 			else
 			{
-				StdDraw.setPenColor(StdDraw.BLUE);
-				StdDraw.filledRectangle(54.5, 56, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 25, 30, 4);
+				StdDraw.setFont(normale);
+				StdDraw.setPenColor(200,200,200);
+				StdDraw.text(54, 22, "Two Players");
+				
 			}
 			
 			// HIGHSCORE :
-			if(StdDraw.mouseX() > 24.5 && StdDraw.mouseX() < 84.5 && StdDraw.mouseY() > 36 && StdDraw.mouseY() < 46) // choix de regarder les meilleurs scores
+			if(StdDraw.mouseX() > 39.5 && StdDraw.mouseX() < 69.5 && StdDraw.mouseY() > 17.1 && StdDraw.mouseY() < 21) // choix de regarder les meilleurs scores
 			{
 				choixDuJoueur = 3;
 				cliqueCase = false;
-				StdDraw.setPenColor(StdDraw.YELLOW);
-				StdDraw.filledRectangle(54.5, 36, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 16, 30, 5);
+				StdDraw.setFont(selection);
+				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.text(54, 12.75, "High Scores");
+				
 			}
 			else
 			{
-				StdDraw.setPenColor(StdDraw.BLUE);
-				StdDraw.filledRectangle(54.5, 36, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 16, 30, 5);
+				StdDraw.setFont(normale);
+				StdDraw.setPenColor(200,200,200);
+				StdDraw.text(54, 13, "High Scores");
 			}
 			
 			// CONTROLS :
-			if(StdDraw.mouseX() > 24.5 && StdDraw.mouseX() < 84.5 && StdDraw.mouseY() > 16 && StdDraw.mouseY() < 26)
+			if(StdDraw.mouseX() > 41 && StdDraw.mouseX() < 66 && StdDraw.mouseY() > 8 && StdDraw.mouseY() < 12)
 			{
 				choixDuJoueur = 5;
 				cliqueCase = false;
-				StdDraw.setPenColor(StdDraw.YELLOW);
-				StdDraw.filledRectangle(54.5, 16, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 6, 30, 5);
+				StdDraw.setFont(selection);
+				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.text(54, 3.7, "Controls");
 			}
 			else
 			{
-				StdDraw.setPenColor(StdDraw.BLUE);
-				StdDraw.filledRectangle(54.5, 16, 30, 5);
+				StdDraw.setPenColor(StdDraw.BLACK);
+				StdDraw.filledRectangle(54.5, 6, 30, 5);
+				StdDraw.setFont(normale);
+				StdDraw.setPenColor(200,200,200);
+				StdDraw.text(54, 4, "Controls");
 			}
 			
 			
 			// QUIT :
-			if(StdDraw.mouseX() > 24.5 && StdDraw.mouseX() < 84.5 && StdDraw.mouseY() > -6 && StdDraw.mouseY() < 6) // choix de quitter
+			if(StdDraw.mouseX() > 47 && StdDraw.mouseX() < 61 && StdDraw.mouseY() > -2 && StdDraw.mouseY() < 4) // choix de quitter
 			{
 				choixDuJoueur = 4;
 				cliqueCase = false;
-				StdDraw.setPenColor(StdDraw.YELLOW);
+				StdDraw.setPenColor(StdDraw.BLACK);
 				StdDraw.filledRectangle(54.5, -4, 30, 5);
+				StdDraw.setFont(selection);
+				StdDraw.setPenColor(StdDraw.WHITE);
+				StdDraw.text(54, -5.2, "Quit");
 			}
 			else
 			{
-				StdDraw.setPenColor(StdDraw.BLUE);
+				StdDraw.setPenColor(StdDraw.BLACK);
 				StdDraw.filledRectangle(54.5, -4, 30, 5);
+				StdDraw.setFont(normale);
+				StdDraw.setPenColor(200,200,200);
+				StdDraw.text(54, -5, "Quit");
 			}
 			
-			StdDraw.setPenColor(StdDraw.WHITE);
-			StdDraw.text(54, 74, "One Player");
-			StdDraw.text(54, 54, "Two Players");
-			StdDraw.text(54, 34, "High Scores");
-			StdDraw.text(54, 14, "Controls");
-			StdDraw.text(54, -5, "Quit");
-			StdDraw.setPenRadius(0.01);
-			StdDraw.setPenColor(StdDraw.GRAY);
-			StdDraw.rectangle(54.5, 76, 30, 5);
-			StdDraw.rectangle(54.5, 56, 30, 5);
-			StdDraw.rectangle(54.5, 36, 30, 5);
-			StdDraw.rectangle(54.5, 16, 30, 5);
-			StdDraw.rectangle(54.5, -4, 30, 5);
-			StdDraw.setPenRadius();
+			
 			StdDraw.show(20);
+			StdDraw.pause(50);
 			}
 		}
 		return choixDuJoueur;
 	}
-	
+
 	public static boolean affichageMenuPause()
 	{
 		boolean choixDuJoueur = true;
-		boolean loop = true;
-		Font font = new Font("SHOWCARD GOTHIC", Font.BOLD, 40);
-		StdDraw.setFont(font);
+		boolean loop = true;;
+		StdDraw.setFont(grande);
 		StdDraw.setPenColor(StdDraw.YELLOW);
-		StdDraw.text(55, 77, "PAUSED");
-		Font font2 = new Font("SHOWCARD GOTHIC", Font.BOLD, 20);
-		StdDraw.setFont(font2);
+		StdDraw.text(55, 77, "PAUSED");;
+		StdDraw.setFont(normale);
 		while(loop)
 		{
 			if(StdDraw.mouseX() > 21.5 && StdDraw.mouseX() < 51.5 && StdDraw.mouseY() > 64 && StdDraw.mouseY() < 74)
@@ -268,19 +294,46 @@ public class GestionDuJeu {
 	
 	public static boolean affichageMenuControl() {
 		boolean stop = true;
-		StdDraw.setCanvasSize(500,760);
+		StdDraw.setCanvasSize(500,760); 
 		StdDraw.setXscale(-2,110);
-		StdDraw.setYscale(-27,149);
+		StdDraw.setYscale(-27,149); 
 		StdDraw.clear(StdDraw.BLACK);
 		StdDraw.picture(54,114,"Images/TitreMenu.png",80,30);
-		Font font = new Font("SHOWCARD GOTHIC", Font.BOLD, 20);
-		StdDraw.setFont(font);
+		
 		StdDraw.setPenColor(StdDraw.WHITE);
-		while(stop)
-		{
-			StdDraw.text(15, 80, "Player 1 :");
-			if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE))
-			{
+		StdDraw.setFont(normale);		
+		StdDraw.text(57, 85, "PLAYER 1 : ");
+		StdDraw.text(57, 65, "PLAYER 2 : ");
+		StdDraw.text(56, 27, "GAME : ");
+						
+		StdDraw.setPenColor(180, 180, 180);
+		StdDraw.setFont(petite);
+		StdDraw.text(55, 78, "ARROW KEYS");
+		StdDraw.text(55, 58, "Z: UP");
+		StdDraw.text(55, 52, "Q: LEFT");
+		StdDraw.text(55, 46, "S: DOWN");
+		StdDraw.text(55, 40, "D: RIGHT");
+		StdDraw.text(55, 20, "SPACE: PAUSE/RESUME");
+		StdDraw.text(55, 14, "ECHAP: EXIT");
+		
+		StdDraw.setPenColor(200, 200, 200);
+		StdDraw.text(55, -4, "GO BACK");
+		
+		
+		while(stop) {
+			stop = true;
+			while(StdDraw.mousePressed() == false) {
+				if (StdDraw.mouseX()>60 && StdDraw.mouseX()<70 && StdDraw.mouseY()>-10 && StdDraw.mouseY()<0) {
+					StdDraw.setPenColor(StdDraw.WHITE);
+					StdDraw.filledRectangle(55, -4, 50, 50);
+					System.out.println("T'es dedans !");
+					
+				} else {
+					
+					
+				}
+			}
+			if(StdDraw.isKeyPressed(KeyEvent.VK_ESCAPE)) {
 				stop = false;
 			}
 			StdDraw.show(20);
